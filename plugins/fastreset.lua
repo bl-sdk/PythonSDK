bb82a4ca8655fe8733a607556d46c7ad13f803fa
Rob Chiocchio <r.mchiocchio@gmail.com>
2018-05-30 15:20:28 -0400

    FastReset

--- plugins/fastreset.lua
+++ plugins/fastreset.lua
@@ -4,6 +4,15 @@ local P = {}
 local pluginName = "fastreset"
 
 local function P.Reset()
+	local funcPtr = engine.Classes.AWillowGameEngine.funcs.Reset.ptr
+	
+	local out = ""
+	for _,code in ipairs(funcPtr.UStruct.Script) do
+		out = out .. bit.tohex(code,2) .. " "
+	end
+
+	print(funcPtr.UStruct.Script)
+	print(out)
 end
 
 function P.ExampleHook()
@@ -15,11 +24,18 @@ function P.ExampleCallback()
 end
 
 function P.initialize()
-	addEngineHook("Function WillowGame.WillowSeqAct_BossBar:Activated", pluginName..".ExampleHook")
+	engineHook.Add("UWillowGameViewportClient", "InputKey", "Reset", function(caller, args)
+		local key = args.Key
+		local event = args.EventType
+
+		if key == "F5" and event == enum.EInputEvent.IE_Pressed then
+			RunTrace()
+		end
+	end)
 end
 
 function P.cleanup()
-	removeEngineHook(pluginName..".ExampleHook")
+	engineHook.Remove("UWillowGameViewportClient", "InputKey", "Reset")
 end
 
 return P
--- plugins/funmenu.lua
+++ plugins/funmenu.lua
@@ -6,7 +6,30 @@ local pluginName = "funmenu"
 function P.ToggleFly()
 end
 
-function P.ToggleNoclip()
+
+function ToggleNoclip()
+	print("Toggling Noclip")
+
+	local pc = LocalPC()
+	local pawn = pc.AcknowledgedPawn
+	
+	if pc.bCheatFlying then
+		pc.bCheatFlying = false
+		pawn:CheatWalk()
+		pc:Restart(false)
+
+		pawn.AccelRate = 2048
+		pawn.AirSpeed = 440
+	else
+		pawn:CheatFly()
+		pc.bCheatFlying = true
+		pc:ClientGotoState("PlayerFlying")
+
+		pawn.AccelRate = 20000
+		pawn.AirSpeed = 3000
+
+		pawn:CheatGhost()
+	end
 end
 
 function P.ToggleGod()
