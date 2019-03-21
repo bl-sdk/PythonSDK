a3e86f52e690915683b96ebff8da48aa6ce7ec04
Matt-Hurd <sabahbob@gmail.com>
2019-03-21 03:21:31 -0500

    Snapshot

--- scripts/split_sdk.py
+++ scripts/split_sdk.py
@@ -13,26 +13,14 @@ void Export_pystes_{}()
 bottom = """  ;
 }"""
 
-files = ['_Structs.cpp']
+files = ['_Structs.cpp', '_Classes.cpp']
 
-dir_path_python = 'C:/Users/abahb/source/repos/BL2-SDK/bl2-sdk/pydefs/'
+dir_path_python = 'C:/Users/abahb/source/repos/BL2-SDK/backup/'
 
 for file in files:
     with open(dir_path_python + file) as f:
         lines = f.readlines()
-        x = -1
-        while x < len(lines):
-            buf = []
-            x += 1
-            line = lines[x]
+        for line in lines:
             if 'class_' in line:
-                class_name = line.split('<')[1].split('>')[0].strip()
-                while ';' not in line:
-                    buf.append(line)
-                    x += 1
-                    line = lines[x]
-                with open(dir_path_python + file.replace('.cpp', '_') + class_name + '.cpp', 'w') as f:
-                    f.write(top.format(class_name))
-                    for line in buf:
-                        f.write(line)
-                    f.write(bottom)
+                class_name = line.split('<')[1].split('>')[0].split(',')[0].strip()
+                print("Export_pystes_{}()".format(class_name))
