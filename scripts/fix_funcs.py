bd738af26b85afacf43e685f271e01e8bb4576d3
Matt-Hurd <sabahbob@gmail.com>
2019-04-03 13:38:50 -0500

    Update fix scripts

--- scripts/fix_funcs.py
+++ scripts/fix_funcs.py
@@ -22,53 +22,22 @@ with open('C:\\SDK_GEN\\BL2\\ObjectsDump.txt') as f:
                     functions[module]['{}.{}'.format(clas, func)]['properties'][prop] = id
 
 
-function_regex = re.compile(r'	g_classFuncs\["(\w+)"\]\["(\w+)"\] = {')
-name_format = 'name = "{}",'
-
-from enum import Enum
-class Location(Enum):
-    EXPECTING_FUNC_START = 0
-    EXPECTING_FIELD_START = 1
-    EXPECTING_FIELD_NAME = 2
-    EXPECTING_FIELD_INDEX = 3
-    EXPECTING_FIELD_OR_END = 4
-    EXPECTING_FUNC_INDEX = 5
-
-
-sdk_dir = 'C:\\Users\\abahb\\source\\repos\\BL2-SDK\\lua\\include\\sdk\\funcs\\'
+sdk_dir = 'C:\\Users\\abahb\\source\\repos\\BL2-SDK\\bl2-sdk\\'
 for module in functions.keys():
     lines = []
-    with open(sdk_dir + module + '.lua') as f:
-            location = Location.EXPECTING_FUNC_START
-            for line in f.readlines():
-                if location == Location.EXPECTING_FUNC_START and 'g_classFuncs' in line:
-                    matches = function_regex.match(line)
-                    if matches:
-                        clas, func = matches.groups()
-                        if func.startswith('event'):
-                            func = func[5:]
-                        if '{}.{}'.format(clas, func) not in functions[module].keys():
-                            clas = clas[1:]
-                        location = Location.EXPECTING_FIELD_START
-                elif location == Location.EXPECTING_FIELD_START and '		fields = {\n' == line:
-                    location = Location.EXPECTING_FIELD_OR_END
-                elif location == Location.EXPECTING_FIELD_OR_END and '			{\n' == line:
-                    location = Location.EXPECTING_FIELD_NAME
-                elif location == Location.EXPECTING_FIELD_NAME and line.startswith('				name = "'):
-                    field_name = line.split('"')[1]
-                    location = Location.EXPECTING_FIELD_INDEX
-                elif location == Location.EXPECTING_FIELD_INDEX and line.startswith('				index = '):
-                    index = line.split(' ')[-1].split(',')[0]
-                    line = line.replace(index, str(int(functions[module]['{}.{}'.format(clas, func)]['properties'][field_name])))
-                    location = Location.EXPECTING_FIELD_OR_END
-                elif location == Location.EXPECTING_FIELD_OR_END and line == '		},\n':
-                    location = Location.EXPECTING_FUNC_INDEX
-                elif location == Location.EXPECTING_FUNC_INDEX and line.startswith('		index = '):
-                    # modify index here
-                    index = line.split(' ')[-1].split(',')[0]
-                    line = line.replace(index, str(int(functions[module]['{}.{}'.format(clas, func)]['id'])))
-                    location = Location.EXPECTING_FUNC_START
-                lines += [line]
-    with open(sdk_dir + module + '.lua', 'w') as f:
+    with open(sdk_dir + module + '_functions.cpp') as f:
+        new_id = None
+        for line in f.readlines():
+            if line[0] != '\t' and '::' in line:
+                clas = line.split('::')[0].split(' ')[-1].strip()[1:]
+            if 'pFn' in line and 'UObject::GObjObjects()->Data' in line:
+                func = clas + '.' + line.split('pFn')[1].split(' ')[0]
+                if func in functions[module].keys():
+                    new_id = functions[module][func]
+                old_id = line.split('[')[1].split(']')[0]
+                print(new_id)
+                line = line.replace(old_id, str(int(new_id['id'])))
+            lines.append(line)
+    with open(sdk_dir + module + '_functions.cpp', 'w') as f:
         for line in lines:
             f.write(line)
