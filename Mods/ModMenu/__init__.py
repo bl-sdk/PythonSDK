53c8d3c61fd99041489a9591179ddcf5ca854d2e
apple1417 <apple1417@users.noreply.github.com>
2020-08-12 10:08:07 +1200

    Extra Small Files (#74)

--- Mods/ModMenu/__init__.py
+++ Mods/ModMenu/__init__.py
@@ -4,7 +4,7 @@ import sys
 
 # Need to define these up here so that they're accessable when importing the other files
 VERSION_MAJOR = 2
-VERSION_MINOR = 0
+VERSION_MINOR = 1
 
 unrealsdk.Log(f"[ModMenu] Version: {VERSION_MAJOR}.{VERSION_MINOR}")
 
