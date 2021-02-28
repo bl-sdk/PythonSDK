e8be8e51b2205fa337fafe2e2b88e744f923ee7e
apple1417 <git@apple1417.dev>
2021-03-01 10:54:30 +1300

    fix backpack manager always being active

--- Mods/BackpackManager/__init__.py
+++ Mods/BackpackManager/__init__.py
@@ -1,6 +1,6 @@
 import unrealsdk
-from Mods.ModMenu import EnabledSaveType, Mods, ModTypes, Options, RegisterMod, SDKMod, Hook
-from Mods import ModMenu
+
+from Mods.ModMenu import EnabledSaveType, Hook, ModTypes, Options, RegisterMod, SDKMod
 
 
 class BackpackManager(SDKMod):
@@ -12,30 +12,23 @@ class BackpackManager(SDKMod):
     Types: ModTypes = ModTypes.Gameplay
     SaveEnabledState: EnabledSaveType = EnabledSaveType.LoadWithSettings
 
-    BackpackSize: ModMenu.Options.Slider = Options.Slider(
+    BackpackSize: Options.Slider = Options.Slider(
         "Backpack", "Change the size of your character's backpack<br>Default is 39", 39, 0, 200, 1
     )
     Options = [BackpackSize]
 
     @Hook("WillowGame.WillowHUD.CreateWeaponScopeMovie")
-    def _GameLoad(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
+    def _GameLoad(self, caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
         PC = unrealsdk.GetEngine().GamePlayers[0].Actor
         if PC and PC.Pawn:
-            PC.Pawn.InvManager.InventorySlotMax_Misc = _ModInstance.BackpackSize.CurrentValue
+            PC.Pawn.InvManager.InventorySlotMax_Misc = self.BackpackSize.CurrentValue
         return True
 
-    def Enable(self) -> None:
-        super().Enable()
-
-    def Disable(self) -> None:
-        ModMenu.RemoveHooks(self)
-
     def ModOptionChanged(self, option, newValue) -> None:
-        if option.Caption == "Backpack":
+        if option == self.BackpackSize:
             PC = unrealsdk.GetEngine().GamePlayers[0].Actor
             if PC and PC.Pawn:
                 PC.Pawn.InvManager.InventorySlotMax_Misc = newValue
 
 
-_ModInstance = BackpackManager()
-RegisterMod(_ModInstance)
+RegisterMod(BackpackManager())
